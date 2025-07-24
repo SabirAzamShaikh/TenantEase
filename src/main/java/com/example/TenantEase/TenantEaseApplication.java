8850aef7a846e437e5d137935b28a8ef77bbd2af
SabirAzamShaikh <www.sabirazamshaikh07@gmail.com>
2025-07-24 18:28:04 +0530

    chanegs in pom.xml

--- src/main/java/com/example/TenantEase/TenantEaseApplication.java
+++ src/main/java/com/example/TenantEase/TenantEaseApplication.java
@@ -2,12 +2,20 @@ package com.example.TenantEase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 //@EnableCaching
-public class TenantEaseApplication {
+public class TenantEaseApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(TenantEaseApplication.class, args);
     }
+//This is been used For Running Our Application On External Tomcat Server
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TenantEaseApplication.class);
+    }
+
 }
